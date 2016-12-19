afc52614a9dad67fc68c8544e3178fbc9a6a159a
squarepantsspongebob <mary2006lucky@sina.com>
2016-12-19 14:07:23 +0800

    Add files via upload
    1. Draw a ring.
    2. Draw a path using a another path: path2.addPath(path1).

---
+++ 1219.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-12-19T09:31:37
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = 1219
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
---
+++ widget.cpp
@@ -0,0 +1,50 @@
+#include "widget.h"
+#include <QPainter>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    setFixedSize (300,400);
+}
+
+void Widget::paintEvent (QPaintEvent *)
+{
+//    QPainterPath path;
+//    path.lineTo(100,200);
+//        path.addEllipse(100,200,50,50);     //向path中添加图形
+//        QPainter painter(this);
+//    painter.setPen(Qt:: blue);
+//    painter.setBrush(Qt::red);
+//        painter.drawPath(path);   //绘制前面创建的path
+//        QPainterPath path2;
+//    path2.addPath(path);   //将前面的path添加到新创建的path中
+//    path2.translate(200,100);
+//        painter.drawPath(path2);
+    int radius = 73.5;
+    QPainter *painter=new QPainter(this);
+    painter->save();
+    painter->setWindow (-150,-200,300,400);
+    painter->setPen(Qt::red);
+    painter->setBrush(Qt::red);
+
+    QPainterPath bigCircle;
+    bigCircle.addEllipse(0-radius, -35-radius, radius * 2, radius * 2);
+    radius = 66.5;
+    QPainterPath smallCircle;
+    smallCircle.addEllipse(0-radius, -35-radius, radius * 2, radius * 2);
+    QPainterPath path1 = bigCircle - smallCircle;
+    painter->drawPath(path1);
+
+
+    bigCircle.addEllipse(0-radius, 35-radius, radius * 2, radius * 2);
+    smallCircle.addEllipse(0-radius, 35-radius, radius * 2, radius * 2);
+    QPainterPath path2 = bigCircle - smallCircle;
+    painter->drawPath(path2);
+
+    painter->restore();
+}
+
+Widget::~Widget()
+{
+
+}
